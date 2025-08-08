e1db46ed0085f682f8b6ad18619c63b03497eaa4
Imre Balogh <60578855+Iminet72@users.noreply.github.com>
2025-08-08 15:12:07 +0200

    Update config_flow.py
    Multi entity

--- custom_components/adap1/config_flow.py
+++ custom_components/adap1/config_flow.py
@@ -3,6 +3,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN, DEFAULT_HOST, DEFAULT_PORT, DEFAULT_UPDATE_INTERVAL
 
@@ -19,11 +20,16 @@ class AdaOkosMeroConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            return self.async_create_entry(title="ADA Okos mérő", data=user_input)
+            existing_names = [entry.title for entry in self._async_current_entries()]
+            if user_input["friendly_name"] in existing_names:
+                errors["friendly_name"] = "name_exists"
+            else:
+                return self.async_create_entry(title=user_input["friendly_name"], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
+                vol.Required("friendly_name"): str,
                 vol.Required("host", default=DEFAULT_HOST): str,
                 vol.Required("port", default=DEFAULT_PORT): int,
                 vol.Required("update_interval", default=DEFAULT_UPDATE_INTERVAL): int,
@@ -41,26 +47,17 @@ class AdaOkosMeroOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow for ADA Okos mérő."""
 
     def __init__(self, config_entry):
-        """Initialize options flow."""
-        # Ne tárold el a config_entry objektumot közvetlenül! 
-        # self.config_entry = config_entry
         self._data = config_entry.data
 
-
-
     async def async_step_init(self, user_input=None):
-        """Manage the options."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                # vol.Required("host", default=self.config_entry.data.get("host")): str,
-                # vol.Required("port", default=self.config_entry.data.get("port")): int,
-                # vol.Required("update_interval", default=self.config_entry.data.get("update_interval")): int,
                 vol.Required("host", default=self._data.get("host")): str,
                 vol.Required("port", default=self._data.get("port")): int,
-                vol.Required("update_interval", default=self._data.get("update_interval")): int,                
+                vol.Required("update_interval", default=self._data.get("update_interval")): int,
             }),
         )
